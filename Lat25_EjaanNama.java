b8576ca6285dc274de898cf6eb77b570c534f521
zalfaNajla2711 <zalfadhianajla@gmail.com>
2024-11-04 14:54:37 +0700

    Add files via upload

---
+++ Lat25_EjaanNama.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lat25_ejaannama;
+
+/**
+ *
+ * @author hp5cd
+ * NAMA: Zalfa Dhia Najla
+ * KELAS: PBO12
+ * NIM: 23176008
+ * PRODI: Sistem Informasi
+ * SEMESTER: 3
+ * Deskripsi Program: Program ini berisi program untuk menampilkan
+ * program ejaan nama
+ *
+ */
+import java.util.Scanner;
+
+public class Lat25_EjaanNama {
+    public static void main(String[] args) {
+        // Scanner to get input from user
+        Scanner scanner = new Scanner(System.in);
+        
+        // Ask user for their name
+        System.out.print("Masukkan nama depan anda untuk di eja: ");
+        String nama = scanner.nextLine();
+        
+        // Print the spelling of the name
+        System.out.println("Ejaan untuk \"" + nama + "\" adalah :");
+        for (int i = 0; i < nama.length(); i++) {
+            System.out.println("Huruf ke-" + (i + 1) + " : " + nama.charAt(i));
+        }
+        
+        // Close scanner to prevent resource leak
+        scanner.close();
+    }
+}
